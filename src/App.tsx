e99e5e515216d663e47bd75ff7a9d1a6e1000f4d
salma o <salmaomran2@gmail.com>
2023-06-13 15:36:28 -0400

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './components/Dashboard.tsx'
+import './components/Tech.tsx'
 import Dashboard from './components/Dashboard';
+import Tech from './components/Tech';
+import Navbar from './components/Navbar.tsx';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <Dashboard/>
+    <Router>
+      <Navbar />
+      <Routes> 
+        <Route path="/" element={<Navigate to="/components/Dashboard"/>} />
+        <Route path="/components/Dashboard" element={<Dashboard />}></Route>
+        <Route path="/components/Tech" element={<Tech />}></Route>
+      </Routes>
+    </Router>
     </div>
   );
 }
